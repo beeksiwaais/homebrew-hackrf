a3b7bbe5d75b2ff188210767ca938b76345c341c
Tobias Heide <theide@stereokrauts.com>
2015-06-28 11:48:59 +0200

    Switch back to 'gr3.6' branch

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GrOsmosdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/GrOsmoSDR'
-  head 'git://git.osmocom.org/gr-osmosdr', :branch => 'master',
+  head 'git://git.osmocom.org/gr-osmosdr', :branch => 'gr3.6',
     :shallow => false
 
   depends_on 'cmake' => :build
